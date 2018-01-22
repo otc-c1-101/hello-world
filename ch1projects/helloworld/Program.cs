f148cacbcdc3521b194ab61bd07ce97645c22540
Unknown <ro0504008@otc.edu>
2018-01-22 12:16:46 -0600

    done

---
+++ ch1projects/helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿//robert oleary
+//cis 150-101
+//1/22/18
+
+using System;
+using static System.Console;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the output
+            Write("Hello World!");
+            Write(" Welcome to C#.\n");
+            //pauses so that you can see it
+            Read();
+        }
+    }
+}
