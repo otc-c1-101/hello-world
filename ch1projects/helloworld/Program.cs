461fd2e00647b5d21a79badc4606a83b367629a9
Unknown <ro0504008@otc.edu>
2018-01-22 12:47:13 -0600

    word doc

--- ch1projects/helloworld/Program.cs
+++ ch1projects/helloworld/Program.cs
@@ -1,18 +1,24 @@
-﻿//robert oleary
-//cis 150-101
-//1/22/18
-
-using System;
+﻿using System;
 using static System.Console;
 
 namespace helloworld
 {
+    /// <summary>
+    /// Program:        Hellos world
+    /// Developer:      robert oleary
+    /// Date:           1/22/18
+    /// Purpose:        say hello
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// this is where the main begins
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //the output
-            Write("Hello World!");
+            Write("hello world!");
             Write(" Welcome to C#.\n");
             //pauses so that you can see it
             Read();
